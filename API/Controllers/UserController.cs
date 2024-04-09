6386b77570502ccb2027f427327ce8d84664a434
Aparna Beemreddy <aparna.beemreddy@gmail.com>
2024-04-08 23:47:54 -0400

    DatingApp: API Initial check in

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(DataContext dataContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetAll()
+    {
+        return await dataContext.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetById(int id)
+    {
+        return await dataContext.Users.FindAsync(id);
+    }
+
+    [HttpPost]
+    public async Task Register(string username, string password)
+    {
+        var newUser = new User
+        {
+            UserName = username,
+            Password = password
+        };
+        await dataContext.Users.AddAsync(newUser);
+    }
+}
---
+++ API/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+	public string UserName { get; set; }
+	public string Password { get; set; }
+	//public string PasswordHash { get; set; }
+	//public string PasswordSalt { get; set; }
+}
