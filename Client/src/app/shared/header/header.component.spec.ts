0a107ed3c92238fe2d02e02650a00f8e2c6abe04
Aparna Beemreddy <aparna.beemreddy@gmail.com>
2024-05-10 15:19:48 -0400

    added side-nav

--- Client/src/app/shared/header/header.component.spec.ts
+++ Client/src/app/shared/header/header.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -19,5 +18,15 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have title`, () => {
+    expect(component.title).toEqual('.Net Web Application with Angular, Web API, & MS SQL');
+  });
+
+  it('should render title', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent)
+      .toContain('.Net Web Application with Angular, Web API, & MS SQL');
   });
 });
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { AppService } from './services/app.service';
 import { LoginComponent } from './login/login.component';
 import { WeatherComponent } from './weather/weather.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, CommonModule, FormsModule, LoginComponent, WeatherComponent, HeaderComponent, FooterComponent]
+  imports: [RouterOutlet, CommonModule, FormsModule, LoginComponent,
+    WeatherComponent, HeaderComponent, FooterComponent, SideNavComponent]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   users: any;
   isLoggedIn = false;
 
   constructor() {
-  }
-
-  ngOnInit(): void {
   }
 }
